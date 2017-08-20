69e5239034b3ce497405a5c3f11f7401b17172e1
Mariola Moreno <mariolamoreno@hotmail.es>
2017-08-20 15:25:03 +0200

    app.set('view engine', 'ejs');

--- spotifyToken.js
+++ spotifyToken.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static(__dirname + '/'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
    res.render("This a service install in the route ./get_token");
